1e123f58505d7cd40f34f6aa90150dfdc9a50e37
DanMarzo <marzogildan@gmail.com>
2022-10-06 21:44:21 -0300

    Usando Loop

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,3 +1,4 @@
+import { AppModule } from './../../app.module';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,10 +10,45 @@ export class HomeComponent implements OnInit {
 
   //public NomeProduto: string = 'Curso do Angular';//modo como criar variavel
   nomeProduto: string = 'Curso do Angular'; //Modo simplificado
-  anuncio: string = `O nome do produto ${this.nomeProduto}`;
+  anuncio: string     = `O  ${this.nomeProduto} em promoção!`;
+  idProduto: number   = 123;
+  precoProduto: number  = 2.59;
+  promocao: boolean   = true;
 
+  constructor() {
+    //this.anuncio = 'O' + this.anuncio + 'está em promoção!';
+    console.log(`Nome do produto: ${this.nomeProduto}`);
+    console.log(`Anuncio: ${this.anuncio}`);
+    console.log(`Id do Produto ${this.idProduto}`);
+    console.log(`Preço produto: ${this.precoProduto}`);
+    console.log(`Promoção ${this.promocao}`);
 
-  constructor() { }
+    //var variavel1;
+    //let variavel2;
+    //const constante1: number = 10;
+
+    //var idade = 19;
+
+    //console.log(`Minha idade é ${idade} anos`);
+
+    let idadeLet = 80;
+
+    console.log(`A idade do Dan é ${idadeLet} anos :D`);
+
+    function ImprimiIdade(){
+      let idade = 20;
+      console.log(`Minha idade ${idade}`);
+    }
+
+    function ImprimiIdadeFor(){
+      for (var idade = 30; idade <= 40; idade++) {
+        console.log(`A idade dentro do FOR :D ${idade}`);
+      }
+      console.log(`A idade fora do FOR =) ${idade}`);
+    }
+    ImprimiIdade();
+    ImprimiIdadeFor();
+  }
 
   ngOnInit(): void {
   }
