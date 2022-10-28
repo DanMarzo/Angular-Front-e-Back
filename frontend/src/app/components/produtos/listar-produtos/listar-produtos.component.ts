5600dd7abcb804476f2a7c9ba994b8ae944d319b
emilly-vitoria <emivi@example.com>
2022-10-27 22:17:21 -0300

    Usando for dentro de uma tag *ngfor

--- frontend/src/app/components/produtos/listar-produtos/listar-produtos.component.ts
+++ frontend/src/app/components/produtos/listar-produtos/listar-produtos.component.ts
@@ -8,11 +8,32 @@ import { Component, OnInit } from '@angular/core';
 export class ListarProdutosComponent implements OnInit {
 
   listaString: string[] = ['Primeiro', 'Segundo', 'Terceiro'];
+  listaNumero: number[] = [15, 15.28, 100]
+
+  objetoModelo = {
+    nome: 'Fatima',
+    idade: 18,
+    altura:1.56,
+    graduado: true
+  };
+
+  listaProduto: any[] = [
+    {nome: 'Curso de Angular', precoProduto: 35.56, validade: '2021-12-31', id: 1},
+    {nome: 'Curso de Ionic', precoProduto: 50, validade: '2021-12-31', id: 2, promocao: true},
+    {id: 3, nome: 'Curso de Ionic Avançado', precoProduto: 50, validade: '2021-12-31'}
+  ];
 
  constructor () {
     for (let item of this.listaString) {
       console.log(item);
     }
+
+    for (let item of this.listaNumero) {
+      console.log(item);
+    }
+
+    console.log(this.objetoModelo);
+    console.log(this.objetoModelo.nome);
   }
 
   ngOnInit(): void {
