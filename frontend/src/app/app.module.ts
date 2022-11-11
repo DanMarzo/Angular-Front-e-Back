b121e13a3047f13051cc71e29a6c60842e61e0e5
DanMarzo <marzogildan@gmail.com>
2022-11-10 21:16:22 -0300

    Adicionando forms

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { FooterComponent } from './template/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { ListarProdutosComponent } from './components/produtos/listar-produtos/listar-produtos.component';
 import { CadastrarProdutosComponent } from './components/produtos/cadastrar-produtos/cadastrar-produtos.component';
+import { FormsModule } from '@angular/forms';
+FormsModule
 
 @NgModule({
   declarations: [
