0141ec9a3d541e2720ed5f923f8bb1b43db4eb64
DanMarzo <marzogildan@gmail.com>
2022-11-24 21:30:00 -0300

    Interface criada

--- frontend/src/app/services/produtos.service.ts
+++ frontend/src/app/services/produtos.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutosService {
+  private URL: String = "http://localhost:3000/produtos";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  buscarTodos(): Observable<any>{
+    return this.http.get(this.URL);
+  }
+
 }
