7f0fbda831a20019d1cc1a94771784aa32195dc6
DanMarzo <marzogildan@gmail.com>
2022-12-05 17:18:32 -0300

    Cadastro de produtos

--- frontend/src/app/components/produtos/cadastrar-produtos/cadastrar-produtos.component.ts
+++ frontend/src/app/components/produtos/cadastrar-produtos/cadastrar-produtos.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IProduto } from 'src/app/Model/IProduto.model';
+import { ProdutosService } from 'src/app/services/produtos.service';
 
 @Component({
   selector: 'app-cadastrar-produtos',
@@ -7,20 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarProdutosComponent implements OnInit {
 
-  nome: string = "";
-  validade: string = '';
-  preco: number = 0;
+  // nome: string = "";
+  // validade: string = '';
+  // preco: number = 0;
+  //Ao usar Interface eu preciso passar parametros
+  produto: IProduto = {
+    nome        : '',
+    validade    : new Date(),
+    precoProduto: 0
+  };
 
-  constructor() { }
+  constructor(private produtoService : ProdutosService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   salvarProduto(): void{
-    console.log('Nome: ', this.nome)
-    console.log('Validade: ', this.validade)
-    console.log('Preço: ', this.preco)
-    alert('Salvo com sucesso!')
+    this.produtoService.cadastrar(this.produto).subscribe(retorno => {
+      this.produto = retorno;
+      this.produtoService.exibirMensagem('Sistema', `${this.produto.nome} foi cadastrado com sucesso. ID: ${this.produto.id}`,'toast-success');
+
+      this.router.navigate(['/produtos']);
+      
+    });
+
+    //console.log('Nome: ', this.nome)
+    //console.log('Validade: ', this.validade)
+    //console.log('Preço: ', this.preco)
+    //alert('Salvo com sucesso!')
   }
 
 }
